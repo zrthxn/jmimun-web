2d23f00e5f5f11c12a09cde9df2935df2ebbe5e9
Alisamar Husain <35369637+zarath1810@users.noreply.github.com>
2019-03-16 22:04:25 +0530

    Materialize

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ const jmimun = express();
 const homepage = express();
 const register = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const ServerConfig = require('./config.json');
 const __domain = require('./config.json').domain;
 
-const Security = require('./util/Security');
+// const Security = require('./util/Security');
 const Gmailer = require('./util/Gmailer');
 const GSheets = require('./util/GSheets');
-const ContentDelivery = require('./util/ContentDelivery');
-const Database = require('./util/Database');
+// const ContentDelivery = require('./util/ContentDelivery');
+// const Database = require('./util/Database');
 
 jmimun.use(cookieParser(ServerConfig.clientKey, {}))
 jmimun.use(bodyParser.json())
@@ -38,24 +38,23 @@ jmimun.listen(PORT, ()=>{
     console.log("Started")
 })
 
-// Static Served Directories
-homepage.use('/static', express.static( path.join(__dirname, 'pages', 'static') ))
-// homepage.use('/register', express.static( path.join(__dirname, 'bookings', 'build') ))
-// homepage.use('/register/main', express.static( path.join(__dirname, 'bookings', 'build') ))
-// homepage.use('/register/payment/', express.static( path.join(__dirname, 'bookings', 'build') ))
-// homepage.use('/register/cancel', express.static( path.join(__dirname, 'bookings', 'build') ))
+// // Static Served Directories
+// homepage.use('/static', express.static( path.join(__dirname, 'pages', 'static') ))
+// // homepage.use('/register', express.static( path.join(__dirname, 'bookings', 'build') ))
+// // homepage.use('/register/main', express.static( path.join(__dirname, 'bookings', 'build') ))
+// // homepage.use('/register/payment/', express.static( path.join(__dirname, 'bookings', 'build') ))
+// // homepage.use('/register/cancel', express.static( path.join(__dirname, 'bookings', 'build') ))
 
-homepage.set('views', path.join(__dirname, 'homepage'))
-homepage.set('view engine', 'hbs')
-homepage.engine('hbs', hbs({
-    defaultLayout: 'main',
-    extname: 'hbs',
-    layoutsDir: __dirname + '/homepage/layouts',
-    partialsDir: [
-        __dirname + '/homepage/partials'
-    ]
-}))
-
+// homepage.set('views', path.join(__dirname, 'homepage'))
+// homepage.set('view engine', 'hbs')
+// homepage.engine('hbs', hbs({
+//     defaultLayout: 'main',
+//     extname: 'hbs',
+//     layoutsDir: __dirname + '/homepage/layouts',
+//     partialsDir: [
+//         __dirname + '/homepage/partials'
+//     ]
+// }))
 
 register.set('views', path.join(__dirname, 'pages', 'forms'))
 register.set('view engine', 'hbs')
@@ -68,27 +67,37 @@ register.engine('hbs', hbs({
     ]
 }))
 
-homepage.get('/unga', (req,res) =>{
-    res.render('unga', {'title': 'General Assembly Delegate Form | JMI International MUN 2019'})
+register.get('/unga', (req,res) =>{
+    res.render('unga', { 'title': 'General Assembly Delegate Form | JMI International MUN 2019' })
 })
 
-homepage.post('_unga', (req,res) => {
-    if(req.body!=null){
-        let data =req.body
-        Gmailer.SingleDataDelivery({
-            to: data.email,
-            from: "thatazimjaved@gmail.com",
-            subject: "Registration received - Number"
-        }, "Registration received", []) 
+register.post('/_register/:comm', (req,res) => {
+    let { comm } = req.params
+    if(req.body!=null) {
+        let data = req.body
         GSheets.AppendToSpreadsheet([{
-            ssId : config.unga.ssid,
-            range: config.unga.sheet,
-            values: [ data.category, data.inst, data.instTypeo, data.age, data.email, data.phone, data.accomodation, data.passport, data.campusAmbassador, data.xp, data.xpDetail, data.pref1, data.pref2, data.pref3, data.payemnt ]
-        }])
-        res.render('success', {'title': "Success | JMI International MUN 2019"})
-        res.sendStatus(200);
-    }
-    else {
+            ssId : config.Sheets[comm].ssId,
+            range: config.Sheets[comm].sheet,
+            values: [ 
+                data.category, data.inst, data.instTypeo, 
+                data.age, data.email, data.phone, 
+                data.accomodation, data.passport, 
+                data.campusAmbassador, data.xp, 
+                data.xpDetail, data.pref1, data.pref2, data.pref3, data.payemnt
+            ]
+        }]).then(()=>{
+            Gmailer.SingleDataDelivery({
+                to: data.email,
+                from: "thatazimjaved@gmail.com",
+                subject: "Registration received - Number"
+            }, "Registration received", [
+                { id: '', data: '' }
+            ]).then(()=>{
+                res.render('success', { 'title': "Success | JMI International MUN 2019" })
+                res.sendStatus(200);
+            })
+        })
+    } else {
         res.render('error', {'title': 'Error | JMI International MUN 2019'})
         res.sendStatus(500)
     }
